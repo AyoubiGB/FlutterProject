66980664a00315b6787f5cb21f1369ce16af1e68
Mohibullah Ayoubi <mohibullahayoubi1@gmail.com>
2025-02-10 14:15:31 +0430

    Add files via upload

---
+++ dart_console_demo/dart_console/bin/dart_console.dart
@@ -0,0 +1,6 @@
+import 'package:dart_console/dart_console.dart' as dart_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console.calculate()}!');
+  print('Hello world Dart');
+}
